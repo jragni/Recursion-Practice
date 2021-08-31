436fdc2d5b44d070f7c8ff5f4bdc5dec380df597
Ray Agni <jagni001@ucr.edu>
2021-08-31 12:32:02 -0700

    Added the longest word length and product solved recursively

---
+++ dsa-recursion/recursion.js
@@ -0,0 +1,50 @@
+/** product: calculate the product of an array of numbers. */
+
+function product(nums, i = 0) {
+  if (i === nums.length) return 1;
+  return nums[i] * product(nums, i + 1);
+}
+
+/** longest: return the length of the longest word in an array of words. */
+
+function longest(words, i = 0, longestWordLength = 0) {
+  if (i === words.length) return longestWordLength;
+  if (words[i].length > longestWordLength) longestWordLength = words[i].length;
+  return longest(words, i + 1, longestWordLength);
+}
+
+/** everyOther: return a string with every other letter. */
+
+function everyOther(str) {}
+
+/** isPalindrome: checks whether a string is a palindrome or not. */
+
+function isPalindrome(str) {}
+
+/** findIndex: return the index of val in arr (or -1 if val is not present). */
+
+function findIndex(arr, val) {}
+
+/** revString: return a copy of a string, but in reverse. */
+
+function revString(str) {}
+
+/** gatherStrings: given an object, return an array of all of the string values. */
+
+function gatherStrings(obj) {}
+
+/** binarySearch: given a sorted array of numbers, and a value,
+ * return the index of that value (or -1 if val is not present). */
+
+function binarySearch(arr, val) {}
+
+module.exports = {
+  product,
+  longest,
+  everyOther,
+  isPalindrome,
+  findIndex,
+  revString,
+  gatherStrings,
+  binarySearch,
+};
